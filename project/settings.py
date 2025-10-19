057d474e9699d3c6351cd3cec0f008a671fbaeaf
unknown <jamicojerahmeel@gmail.com>
2025-10-19 10:49:37 +0100

    feat: update settings.py

--- project/settings.py
+++ project/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+ALLOWED_HOSTS = ["*"]
 
-STATIC_URL = 'static/'
+# Static files settings (required for Railway)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+
+ 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
